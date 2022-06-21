ad32230658dd374d74fae4b4bae2b409260bd337
dapoadedire <adedireadedapo19@gmail.com>
2022-06-21 01:55:23 +0100

    Add semi colon

--- Intro1/script.js
+++ Intro1/script.js
@@ -1,43 +1,43 @@
-alert("This is a calculator for you. Please enter the appropriate parameters. This calculator can only perform operations between 2 numbers please.")
+alert("This is a calculator for you. Please enter the appropriate parameters. This calculator can only perform operations between 2 numbers please.");
 
 
 
 function add(a, b) {
-    alert("Result: "+ (a) + " + " + (b) + " = " + (a + b))
+    alert("Result: "+ (a) + " + " + (b) + " = " + (a + b));
 }
 
 function subtract(a, b) {
-    alert("Result: "+ (a) + " - " + (b) + " = " + (a - b))
+    alert("Result: "+ (a) + " - " + (b) + " = " + (a - b));
 }
 
 
 function divide(a, b) {
-    alert("Result: " + (a) + " / " + (b) + " = " + (a / b))
+    alert("Result: " + (a) + " / " + (b) + " = " + (a / b));
 }
 
 function multiply(a, b) {
-    alert("Result: "+ (a) + " * " + (b) + " = " + (a * b))
+    alert("Result: "+ (a) + " * " + (b) + " = " + (a * b));
 }
 
 
 function calculate(a, b, operation) {
     if (operation == "+") {
-        add(a, b)
+        add(a, b);
     } else if (operation == "-") {
-        subtract(a, b)
+        subtract(a, b);
     } else if (operation == "/") {
-        divide(a, b)
+        divide(a, b);
     } else if (operation == "*") {
-        multiply(a, b)
+        multiply(a, b);
     } else {
-        alert("Please enter a valid operation.")
+        alert("Please enter a valid operation.");
     }
 }
 
-let a = parseFloat(prompt("Please enter the first number: "))
-let b = parseFloat(prompt("Please enter the second number: "))
-let operation = prompt("Please enter the operation you would like to perform: add +, subtract -, divide /, multiply * ")
+let a = parseFloat(prompt("Please enter the first number: "));
+let b = parseFloat(prompt("Please enter the second number: "));
+let operation = prompt("Please enter the operation you would like to perform: add +, subtract -, divide /, multiply * ");
 
-calculate(a, b, operation)
+calculate(a, b, operation);
 
 
